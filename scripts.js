9d08469f0d430b4cd3da7cb95912c64db9b30e32
baltaz <baltazar.crstn@gmail.com>
2021-04-16 12:22:15 -0300

    responsive layout

--- scripts.js
+++ scripts.js
@@ -1,54 +1,51 @@
 let greetings = ["Hello world", "Hi there", "Welcome!" , "Hola!", "Nice to meet you!", "Nice to see you!", "Hi!", "Hiya!"];
 const getRamdomFrom = elems => elems[Math.floor(Math.random()*elems.length)];
 
-//////////////////////
-
+/* cursor creation */
 const cursor = document.createElement("span");
 cursor.classList.add("cursor");
 cursor.textContent = '\xa0';
 
-
+/* configurable params */
 const typingDelay = 16;
-
-let textArrayIndex = 0;
-let charIndex = 0;
-let bubble = document.querySelector(".bubble");
-
-const elements = [".greetings",".say-my-name",".about-me"];
+const elements = [".greetings",".name",".bio"];
 const texts = ["<h1>"+getRamdomFrom(greetings)+"</h1>","<h3>I'm Cristian Baltazar</h3>", "<p>I have more than 10 years working on the creation of digital products. Curious and restless, I am passionate about the web, the constant evolution and new trends in technology, usability and design, give frame to that world full of dynamism and challenges that I love to be part of .</p>"]
 
+/* global variables */
+let arrayIndex = 0;
+let charIndex = 0;
+let bubble = document.getElementById("bubble");
 
-function formatCode(typedTextSpan) {
-  typedTextSpan.textContent = typedTextSpan.textContent.replace(/<\/?[^>]+(>|$)/g, "");
-  typedTextSpan.classList.add("done");
+function formatCode(element) {
+  element.textContent = element.textContent.replace(/<\/?[^>]+(>|$)/g, "");
+  element.classList.add("done");
   charIndex = 0;
-  if(++textArrayIndex < texts.length)
-    setTimeout(()=>typeContent(texts[textArrayIndex],elements[textArrayIndex]), 400)
+  if(++arrayIndex < texts.length)
+    setTimeout(()=>typeContent(texts[arrayIndex],elements[arrayIndex]), 400)
   else bubble.classList.remove("disable");
 }
 
-function typeName(texto,typedTextSpan) {
+function justTyping(texto,element) {
   if (charIndex < texto.length) {
     if(!cursor.classList.contains("typing")) cursor.classList.add("typing");
-    typedTextSpan.textContent += texto.charAt(charIndex);
+    element.textContent += texto.charAt(charIndex);
     charIndex++;
-    setTimeout(()=>typeName(texto,typedTextSpan), typingDelay);
+    setTimeout(()=>justTyping(texto,element), typingDelay);
   }else{
     cursor.classList.remove("typing");
-    setTimeout(()=>formatCode(typedTextSpan), 500);
+    setTimeout(()=>formatCode(element), 500);
   }
 }
 
 const typeContent = (texto,elem)=>{
-  const typedTextSpan = document.querySelector(elem);
-  typedTextSpan.parentElement.appendChild(cursor);
+  const element = document.querySelector(elem);
+  element.parentElement.appendChild(cursor);
   bubble.classList.add("disable");
-  typeName(texto, typedTextSpan);
+  justTyping(texto, element);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-
-  typeContent(texts[textArrayIndex],elements[textArrayIndex]);
+  typeContent(texts[arrayIndex],elements[arrayIndex]);
   
   bubble.addEventListener("click", ()=>{
     elements.forEach(e=>{
@@ -56,11 +53,10 @@ document.addEventListener("DOMContentLoaded", function() {
       element.textContent=""
       element.classList.remove("done");
     });
-    textArrayIndex =0;
-    typeContent(texts[textArrayIndex],elements[textArrayIndex])
+    arrayIndex = 0;
+    typeContent(texts[arrayIndex],elements[arrayIndex])
   }) 
 });
 
 // 2) Destacar el Strong
-// 3) hacer responsive la web
 // 4) encontrar la forma de que los textos preexistan en el HTML (por SEO);
